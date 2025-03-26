137a4fe083d6adf3eb2675e46e0f46f0e13ce703
Felix Bronnhuber <bronfe01@thu.de>
2025-03-26 19:47:08 +0100

    Added custom mappings file with toggle term and git keymaps

--- init.lua
+++ init.lua
@@ -196,6 +196,9 @@ vim.keymap.set('n', '<C-l>', '<C-w><C-l>', { desc = 'Move focus to the right win
 vim.keymap.set('n', '<C-j>', '<C-w><C-j>', { desc = 'Move focus to the lower window' })
 vim.keymap.set('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper window' })
 
+-- Import from lua/mappings.lua
+require 'mappings'
+
 -- [[ Basic Autocommands ]]
 --  See `:help lua-guide-autocommands`
 
---
+++ lua/mappings.lua
@@ -0,0 +1,28 @@
+local map = vim.keymap.set
+
+-- Closes the buffer
+map('n', '<leader>x', function()
+  require('nvchad.tabufline').close_buffer()
+end, { desc = 'buffer close' })
+
+-- toggleable
+map({ 'n', 't' }, '<A-v>', function()
+  require('nvchad.term').toggle { pos = 'vsp', id = 'vtoggleTerm' }
+end, { desc = 'terminal toggleable vertical term' })
+
+map({ 'n', 't' }, '<A-h>', function()
+  require('nvchad.term').toggle { pos = 'sp', id = 'htoggleTerm' }
+end, { desc = 'terminal toggleable horizontal term' })
+
+map({ 'n', 't' }, '<A-i>', function()
+  require('nvchad.term').toggle { pos = 'float', id = 'floatTerm' }
+end, { desc = 'terminal toggle floating term' })
+
+-- Telescope Git
+map('n', '<leader>gs', function()
+  require('telescope.builtin').git_status()
+end, { desc = 'git status' })
+
+map('n', '<leader>gc', function()
+  require('telescope.builtin').git_commits()
+end, { desc = 'git commits' })
