7bbab96bc11e345258d2f898c9d8e1aa5f8b2612
Felix Bronnhuber <felix.bronnhuber@nokia.com>
2025-03-26 19:14:50 +0100

    Added snacks. Minor changes.

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -2,4 +2,15 @@
 --  I promise not to create any merge conflicts in this directory :)
 --
 -- See the kickstart.nvim README for more information
-return {}
+return {
+  {
+    'folke/snacks.nvim',
+    priority = 1000,
+    lazy = false,
+    ---@type snacks.Config
+    opts = {
+      bigfile = { enabled = true },
+      scroll = { enabled = true },
+    },
+  },
+}
