03ae7ebcc0ca3cab0000cddb96a3b6c53c15d92e
Felix Bronnhuber <bronfe01@thu.de>
2025-05-06 22:50:36 +0200

    Added vimtex with zathura as viewer.

---
+++ lua/custom/plugins/vimtex.lua
@@ -0,0 +1,15 @@
+return {
+  'lervag/vimtex',
+  lazy = false, -- we don't want to lazy load VimTeX
+  -- tag = "v2.15", -- uncomment to pin to a specific release
+  init = function()
+    -- VimTeX configuration goes here, e.g.
+    -- if on macos:
+    -- if vim.fn.has 'mac' == 1 then
+    vim.g.vimtex_view_method = 'zathura_simple'
+    vim.g.vimtex_view_zathura_use_synctex = 1
+    -- else
+    --   vim.g.vimtex_view_method = 'default'
+    -- end
+  end,
+}
