4ccbceff182c81fc0a43cd148f221b622acb131c
Felix Bronnhuber <felix.bronnhuber@nokia.com>
2025-09-18 11:01:18 +0200

    Added theme toggle (light with rose-pine and dark with vague) and streamlined icons between lualine, bufferline, fidget and diagnostics

--- init.lua
+++ init.lua
@@ -23,6 +23,11 @@ vim.keymap.set('n', '<leader>W', ':wa<CR>:qa<CR>')
 vim.keymap.set('n', '<leader>f', vim.lsp.buf.format)
 vim.keymap.set('n', '<leader>r', ':make<CR>')
 vim.keymap.set('n', '<leader>x', ':bd<CR>')
+vim.keymap.set('n', '<leader>y', '"+y')
+vim.keymap.set('n', '<leader>ya', function()
+	vim.cmd('normal! ggVG"+y')
+	vim.notify('Yanked buffer to clipboard', vim.log.levels.INFO)
+end, { desc = 'Yank entire buffer to clipboard with message' })
 
 vim.pack.add({
 	{ src = "https://github.com/tpope/vim-sleuth.git" },
@@ -30,6 +35,7 @@ vim.pack.add({
 	{ src = "https://github.com/folke/neodev.nvim.git" },
 	{ src = "https://github.com/nvim-treesitter/nvim-treesitter.git" },
 	{ src = "https://github.com/vague2k/vague.nvim.git" },
+	{ src = "https://github.com/rose-pine/neovim.git" },
 	{ src = "https://github.com/Saghen/blink.cmp.git" },
 	{ src = "https://github.com/lewis6991/gitsigns.nvim.git" },
 	{ src = "https://github.com/nvim-telescope/telescope.nvim.git" },
@@ -57,6 +63,18 @@ end, { desc = 'Toggle floating terminal' })
 
 require("vague").setup { transparent = true, style = { strings = "none" }, }
 vim.cmd("colorscheme vague")
+require("rose-pine").setup { variant = 'dawn' }
+
+-- Toggle between 'rose-pine-dawn' and 'vague' colorschemes
+vim.api.nvim_set_keymap('n', '<Space>tt', [[:lua ToggleTheme()<CR>]], { noremap = true, silent = true })
+function ToggleTheme()
+	local current = vim.g.colors_name
+	if current == 'rose-pine' then
+		vim.cmd('colorscheme vague')
+	else
+		vim.cmd('colorscheme rose-pine')
+	end
+end
 
 require("neodev").setup()
 local lspconfig = require('lspconfig')
@@ -81,6 +99,16 @@ for _, lsp in ipairs(servers) do
 	}
 end
 
+local icons = {
+	ERROR = '󰅚 ',
+	WARN = '󰀪 ',
+	INFO = '󰋽 ',
+	HINT = '󰌶 ',
+	DEBUG = ' ',
+	NOTIFICATION = ' ',
+	DONE = ' ',
+}
+
 vim.diagnostic.config({
 	virtual_lines = false,
 	virtual_text = true,
@@ -90,10 +118,10 @@ vim.diagnostic.config({
 	float = { source = true, },
 	signs = {
 		text = {
-			[vim.diagnostic.severity.ERROR] = "󰅚 ",
-			[vim.diagnostic.severity.WARN] = "󰀪 ",
-			[vim.diagnostic.severity.INFO] = "󰋽 ",
-			[vim.diagnostic.severity.HINT] = "󰌶 ",
+			[vim.diagnostic.severity.ERROR] = icons.ERROR,
+			[vim.diagnostic.severity.WARN] = icons.WARN,
+			[vim.diagnostic.severity.INFO] = icons.INFO,
+			[vim.diagnostic.severity.HINT] = icons.HINT,
 		},
 		numhl = {
 			[vim.diagnostic.severity.ERROR] = "ErrorMsg",
@@ -150,9 +178,8 @@ vim.keymap.set('n', '<leader>cc', ':CodeCompanionChat<CR>', { desc = 'Open CodeC
 
 require("typst-preview").setup {}
 vim.keymap.set('n', '<leader>vt', ':TypstPreview<CR>', { desc = 'View typst preview' })
-
 require('lualine').setup {
-	options = { section_separators = {left = '', right = '' }, component_separators = '' }
+	options = { section_separators = '', component_separators = '│' }
 }
 
 require("no-neck-pain").setup { width = 120, mappings = { enabled = true } }
@@ -171,19 +198,27 @@ require("snacks").setup {
 
 require("fidget").setup {
 	notification = {
+		configs = {
+			default = vim.tbl_extend("force", require("fidget.notification").default_config, {
+				icon = icons.NOTIFICATION,
+				debug_annote = icons.DEBUG,
+				error_annote = icons.ERROR,
+				info_annote = icons.INFO,
+				warn_annote = icons.WARN,
+			}),
+		},
 		override_vim_notify = true,
-		window = {
-			winblend = 0,
-			border = "rounded"
-		}
-	}
+		window = { winblend = 0, border = "rounded" },
+	},
+	progress = { display = { done_icon = icons.DONE } },
 }
 
 local bufferline = require("bufferline")
 bufferline.setup {
 	options = {
 		indicator = { style = 'none' },
-		separator_style = { '', '' },
+		separator_style = { '│', '│' },
 		show_buffer_close_icons = false,
 	}
 }
+vim.keymap.set('n', '<leader>M', ':Fidget history<CR>')
